cbe78ca494c2310611e7b1e40c6152b9b09394ef
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-04-13 21:45:16 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('form.feedback-form');
+const inputEmail = document.querySelector('label > input');
+const inputText = document.querySelector('label > textarea');
 
+const formDataString = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (formDataString) {
+  inputEmail.value = formDataString.email;
+  inputText.value = formDataString.message;
+}
+
+form.addEventListener('input', () => {
+  const data = {
+    email: inputEmail.value,
+    message: inputText.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+});
+
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({
+    email: inputEmail.value,
+    message: inputText.value,
+  });
+  inputEmail.value = '';
+  inputText.value = '';
+  localStorage.removeItem('feedback-form-state');
+});
